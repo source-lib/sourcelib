6908290fc302ef4c723a0c6a269c2fd229907cd4
Stefan Heinz <stefan.heinz.02@gmail.com>
2022-09-14 00:54:33 +0200

    Add keyvalue project based off source engine support vscode extension

---
+++ kv/src/shader-param.ts
@@ -0,0 +1,29 @@
+export class ShaderParam {
+
+	public name: string;
+	public type: string;
+	public defaultCompletion?: string | boolean | number;
+	public description?: string;
+	public wikiUri?: string;
+
+	constructor(name: string, type: string, defaultCompletion: string, description: string, wikiUri: string) {
+		this.name = name;
+		this.type = type;
+		this.defaultCompletion = defaultCompletion;
+		this.description = description;
+		this.wikiUri = wikiUri;
+	}
+
+}
+
+export class ShaderParamHint {
+
+	public paramName: string;
+	public valueRegex: RegExp;
+
+	constructor(paramName: string, valueRegex: RegExp) {
+		this.paramName = paramName;
+		this.valueRegex = valueRegex;
+	}
+
+}
