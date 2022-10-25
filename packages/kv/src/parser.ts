766f82ffc0c20175ca41faf2346bf0414558639b
Stefan Heinz <stefan.heinz.02@gmail.com>
2022-10-25 09:01:49 +0200

    Refactor kv parser

--- packages/kv/src/parser.ts
+++ packages/kv/src/parser.ts
@@ -1,6 +1,16 @@
 import { tokenize } from "./tokenizer";
 import { Conditional, Document, Item, ParseError, ParseErrorType, Position, Literal, Range, Token, TokenList, TokenType } from "./parser-types";
 
+interface ParserState {
+    currentParent: Item | null;
+    keyToken: Token | null;
+    valueTokens: Token[];
+    conditionToken: Token | null;
+
+    errors: Array<ParseError>;
+    roots: Array<Item>;
+}
+
 export function parseText(text: string): Document {
     const tokens = tokenize(text);
     const document = parseTokens(tokens);
@@ -8,121 +18,133 @@ export function parseText(text: string): Document {
 }
 
 export function parseTokens(tokens: TokenList): Document {
-    
-    const errors = new Array<ParseError>();
-    const roots = new Array<Item>();
 
-    let currentParent: Item | null = null;
-    let keyToken: Token | null = null;
-    let valueTokens: Token[] = [];
-    let conditionToken: Token | null = null;
+    const state = {
+        conditionToken: null,
+        currentParent: null,
+        keyToken: null,
+        valueTokens: [],
+        errors: new Array<ParseError>(),
+        roots: new Array<Item>()
+    } as ParserState;
 
     for(const token of tokens) {
         // Ignore comments
-        if(token.type === TokenType.Comment) {
+        if(token.type === TokenType.Comment)
             continue;
-        }
 
         // KV set is done
-        if(keyToken != null && keyToken.line !== token.line) {
-            if(valueTokens.length == 0) {
-                
-                const error = new ParseError(ParseErrorType.MissingValue, keyToken.getPosition());
-                errors.push(error);
-            }
-
-            if(currentParent == null) {
-                let end: number; // This code is copy pasted from down below. Definitely need to refactor this parser
-                if(valueTokens.length > 0) {
-                    end = valueTokens[valueTokens.length - 1].range.getEnd();
-                } else {
-                    end = keyToken.range.getEnd();
-                }
-                const error = new ParseError(ParseErrorType.MissingRootObject, new Position(keyToken.line, new Range(keyToken.range.getStart(), end)));
-                errors.push(error);
-            } else {
-                const key = keyToken.toLiteral();
-                const values = valueTokens.map(t => new Literal(new Position(t.line, t.range), t.value));
-                const condition: Conditional | null = conditionToken == null ? null : conditionToken.toConditional();
-                const item = Item.createLeaf(currentParent, key, values, condition);
-                currentParent.addChild(item);
-            }
-
-            keyToken = null;
-            valueTokens = [];
-            conditionToken = null;
-        }
+        completeOutstandingKvSet(state, token);
 
         if(token.type === TokenType.Key) {
-            keyToken = token;
-            valueTokens = [];
+            state.keyToken = token;
+            state.valueTokens = [];
             continue;
         }
 
         if(token.type === TokenType.Value) {
-            if(keyToken == null) {
+            if(state.keyToken == null) {
                 // This should never happen, but just in case.
                 continue;
             }
 
-            valueTokens.push(token);
+            state.valueTokens.push(token);
             continue;
         }
 
         if(token.type === TokenType.ObjectStart) {
-            if(keyToken == null) {
+            if(state.keyToken == null) {
                 const error = new ParseError(ParseErrorType.UnexpectedOpeningBrace, token.getPosition());
-                errors.push(error);
+                state.errors.push(error);
                 continue;
             }
-            const key = keyToken.toLiteral();
-            const condition: Conditional | null = conditionToken == null ? null : conditionToken.toConditional();
-            const item = Item.createContainer(currentParent, key, [], condition);
+            const key = state.keyToken.toLiteral();
+            const condition: Conditional | undefined = state.conditionToken?.toConditional();
+            const item = Item.createContainer(state.currentParent, key, [], condition);
             item.startPopulatingContainer(token.toLiteral());
 
-            if(currentParent == null) {
-                currentParent = item;
-                roots.push(item);
+            if(state.currentParent == null) {
+                state.currentParent = item;
+                state.roots.push(item);
             } else {
-                currentParent.addChild(item);
-                currentParent = item;
+                state.currentParent.addChild(item);
+                state.currentParent = item;
             }
 
-            keyToken = null;
-            conditionToken = null;
+            state.keyToken = null;
+            state.conditionToken = null;
             continue;
         }
 
         if(token.type === TokenType.ObjectEnd) {
-            if(currentParent == null) {
+            if(state.currentParent == null) {
                 const error = new ParseError(ParseErrorType.UnexpectedClosingBrace, token.getPosition());
-                errors.push(error);
+                state.errors.push(error);
             } else {
-                currentParent.startPopulatingContainer(token.toLiteral());
-                currentParent = currentParent.getParent();
+                state.currentParent.startPopulatingContainer(token.toLiteral());
+                state.currentParent = state.currentParent.getParent();
             }
 
             continue;
         }
 
         if(token.type === TokenType.Conditional) {
-            conditionToken = token;
+            state.conditionToken = token;
             continue;
         }
 
     }
 
-    if(keyToken != null) {
-        if(currentParent == null) {
-            let end: number;
-            if(valueTokens.length > 0) {
-                end = valueTokens[valueTokens.length - 1].range.getEnd();
-            } else {
-                end = keyToken.range.getEnd();
+    if(state.keyToken != null) {
+        if(state.currentParent == null) {
+            const pos = getKvSetPosition(state);
+            if(pos != null) {
+                state.errors.push(new ParseError(ParseErrorType.MissingRootObject, pos));
             }
-            errors.push(new ParseError(ParseErrorType.MissingRootObject, new Position(keyToken.line, new Range(keyToken.range.getStart(), end))));
         }
     }
 
-    return new Document(roots, errors);
+    return new Document(state.roots, state.errors);
+}
+
+function getKvSetPosition(s: ParserState): Position | null {
+    
+    if(s.keyToken == null) return null;
+    let end: number;
+    
+    if(s.valueTokens.length > 0) {
+        end = s.valueTokens[s.valueTokens.length - 1].range.getEnd();
+    } else {
+        end = s.keyToken.range.getEnd();
+    } 
+
+    const range = new Range(s.keyToken.range.getStart(), end);
+    return new Position(s.keyToken.line, range);
+}
+
+function completeOutstandingKvSet(s: ParserState, token: Token): void {
+    if(s.keyToken != null && s.keyToken.line !== token.line) {
+        if(s.valueTokens.length == 0) {
+            
+            const error = new ParseError(ParseErrorType.MissingValue, s.keyToken.getPosition());
+            s.errors.push(error);
+        }
+
+        if(s.currentParent == null) {
+            const pos = getKvSetPosition(s);
+            if(pos != null) {
+                s.errors.push(new ParseError(ParseErrorType.MissingRootObject, pos));
+            }
+        } else {
+            const key = s.keyToken.toLiteral();
+            const values = s.valueTokens.map(t => new Literal(new Position(t.line, t.range), t.value));
+            const condition: Conditional | undefined = s.conditionToken?.toConditional();
+            const item = Item.createLeaf(s.currentParent, key, values, condition);
+            s.currentParent.addChild(item);
+        }
+
+        s.keyToken = null;
+        s.valueTokens = [];
+        s.conditionToken = null;
+    }
 }
