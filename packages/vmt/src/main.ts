faae43e2f7dae1eb4b2039ca197fa9a24d0ec05e
Stefan Heinz <stefan.heinz.02@gmail.com>
2023-03-18 16:14:55 +0100

    Export shader params in vmt package

--- packages/vmt/src/main.ts
+++ packages/vmt/src/main.ts
@@ -1,9 +1,11 @@
 import { ShaderParam, ShaderParamHint } from "./shader-param";
+import { shaderParams,  internalTextures } from "./shader-param-list";
 import { ColorMatchDescription, ColorMatchParenthesisType, getColorMatches } from "./color";
 import { getMatrixMatches, matrixRegExp } from "./matrix";
 
 export {
     ShaderParam, ShaderParamHint,
     ColorMatchDescription, ColorMatchParenthesisType, getColorMatches,
-    getMatrixMatches, matrixRegExp
+    getMatrixMatches, matrixRegExp,
+    shaderParams, internalTextures
 };
